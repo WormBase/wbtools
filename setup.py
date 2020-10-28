204b50951b432b54464b98d32951ed476086b25a
Valerio Arnaboldi <valearna@caltech.edu>
2020-10-28 11:25:14 -0700

    fix(setup): added explicit package dir

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/valearna/wbtools",
+    package_dir={'': 'wbtools'},
     packages=setuptools.find_packages('wbtools'),
     classifiers=[
         "Programming Language :: Python :: 3",
