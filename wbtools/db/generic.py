0f3b4e6c2f297678527b893a488158b920f84cd9
Valerio Arnaboldi <valearna@caltech.edu>
2021-05-05 10:54:13 -0700

    fix(db): added function to get antibody str value

--- wbtools/db/generic.py
+++ wbtools/db/generic.py
@@ -248,6 +248,16 @@ class WBGenericDBManager(AbstractWBDBManager):
                          "and cur_timestamp >= %(from_date)s", {'from_date': from_date})
             return [(row[0], row[1]) for row in curs.fetchall()]
 
+    def get_antibody_str_value(self, paper_id):
+        with psycopg2.connect(self.connection_str) as conn, conn.cursor() as curs:
+            curs.execute("SELECT cur_strdata FROM cur_strdata where cur_datatype = 'antibody' "
+                         "and cur_paper = %(paper_id)s ORDER BY cur_timestamp DESC", {'paper_id': paper_id})
+            res = curs.fetchone()
+            if res:
+                return res[0]
+            else:
+                return None
+
     def save_antybody_str_values(self, paper_id, str_values):
         with psycopg2.connect(self.connection_str) as conn, conn.cursor() as curs:
             curs.execute("INSERT INTO cur_strdata (cur_paper, cur_datatype, cur_strdata) "
