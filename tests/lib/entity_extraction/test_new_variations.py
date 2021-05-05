8092fe815c080f216763f46d26371c917c3c2284
Valerio Arnaboldi <valearna@caltech.edu>
2021-05-05 11:03:19 -0700

    test(ntt_extraction): fixed method call in tests

--- tests/lib/entity_extraction/test_new_variations.py
+++ tests/lib/entity_extraction/test_new_variations.py
@@ -21,19 +21,21 @@ class TestNLPNewVariations(unittest.TestCase):
         self.ntt_extractor = NttExtractor(db_manager=db_manager)
 
     def test_get_new_variations_from_text(self):
-        variations = self.ntt_extractor.extract_entities(self.text, EntityType.VARIATION, match_curated=False,
-                                                         exclude_curated=True)
+        variations = self.ntt_extractor.extract_all_entities_by_type(self.text, EntityType.VARIATION,
+                                                                     match_curated=False,
+                                                                     exclude_curated=True)
         self.assertGreater(len(variations), 0)
         text = "The result- ing ez72syb1455 animals show the exact same gfp expression pattern as that of the " \
                "parental ez72 strain and also display no mutant phenotype (Figures S2A–S2C), thereby supporting past " \
                "evidence [8] that the tra-1b isoform bears no relevant Tra function."
-        variations = self.ntt_extractor.extract_entities(text, EntityType.VARIATION, match_curated=False,
-                                                         exclude_curated=True)
+        variations = self.ntt_extractor.extract_all_entities_by_type(text, EntityType.VARIATION, match_curated=False,
+                                                                     exclude_curated=True)
         self.assertTrue("ez72syb1455" in variations)
 
     def test_is_variation_suspicious(self):
-        variations = self.ntt_extractor.extract_entities(self.text, EntityType.VARIATION, match_curated=False,
-                                                         exclude_curated=True)
+        variations = self.ntt_extractor.extract_all_entities_by_type(self.text, EntityType.VARIATION,
+                                                                     match_curated=False,
+                                                                     exclude_curated=True)
         is_susp = is_variation_suspicious(variations[0])
         self.assertEqual(is_susp, "potentially new")
 
