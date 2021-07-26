aedd792281db1b6a3101f3bb23da792f13836c83
Valerio Arnaboldi <valearna@caltech.edu>
2021-07-26 17:30:03 +0200

    fix(db): fixed function to get curated papers

--- wbtools/db/afp.py
+++ wbtools/db/afp.py
@@ -8,6 +8,7 @@ from wbtools.db.abstract_manager import AbstractWBDBManager
 from wbtools.db.generic import WBGenericDBManager
 from wbtools.db.paper import WBPaperDBManager
 from wbtools.db.person import WBPersonDBManager
+from wbtools.lib.scraping import get_curated_papers
 
 QUERY_PAPER_IDS_NO_SUBMISSION = "SELECT distinct afp_ve.joinkey FROM afp_version afp_ve FULL OUTER JOIN afp_genestudied afp_g ON afp_ve.joinkey = afp_g.joinkey FULL OUTER JOIN afp_species afp_s ON afp_ve.joinkey = afp_s.joinkey FULL OUTER JOIN afp_variation afp_v ON afp_ve.joinkey = afp_v.joinkey FULL OUTER JOIN afp_strain afp_st ON afp_ve.joinkey = afp_st.joinkey FULL OUTER JOIN afp_transgene afp_t ON afp_ve.joinkey = afp_t.joinkey FULL OUTER JOIN afp_seqchange afp_seq ON afp_ve.joinkey = afp_seq.joinkey FULL OUTER JOIN afp_geneint afp_ge ON afp_ve.joinkey = afp_ge.joinkey FULL OUTER JOIN afp_geneprod afp_gp ON afp_ve.joinkey = afp_gp.joinkey FULL OUTER JOIN afp_genereg afp_gr ON afp_ve.joinkey = afp_gr.joinkey FULL OUTER JOIN afp_newmutant afp_nm ON afp_ve.joinkey = afp_nm.joinkey FULL OUTER JOIN afp_rnai afp_rnai ON afp_ve.joinkey = afp_rnai.joinkey FULL OUTER JOIN afp_overexpr afp_ov ON afp_ve.joinkey = afp_ov.joinkey FULL OUTER JOIN afp_structcorr afp_stc ON afp_ve.joinkey = afp_stc.joinkey FULL OUTER JOIN afp_antibody ON afp_ve.joinkey = afp_antibody.joinkey FULL OUTER JOIN afp_siteaction ON afp_ve.joinkey = afp_siteaction.joinkey FULL OUTER JOIN afp_timeaction ON afp_ve.joinkey = afp_timeaction.joinkey FULL OUTER JOIN afp_rnaseq ON afp_ve.joinkey = afp_rnaseq.joinkey FULL OUTER JOIN afp_chemphen ON afp_ve.joinkey = afp_chemphen.joinkey FULL OUTER JOIN afp_envpheno ON afp_ve.joinkey = afp_envpheno.joinkey FULL OUTER JOIN afp_catalyticact ON afp_ve.joinkey = afp_catalyticact.joinkey FULL OUTER JOIN afp_humdis ON afp_ve.joinkey = afp_humdis.joinkey FULL OUTER JOIN afp_additionalexpr ON afp_ve.joinkey = afp_additionalexpr.joinkey FULL OUTER JOIN afp_comment ON afp_ve.joinkey = afp_comment.joinkey WHERE afp_ve.afp_version = '2' AND afp_g.afp_genestudied IS NULL AND afp_s.afp_species IS NULL AND afp_v.afp_variation IS NULL AND afp_st.afp_strain IS NULL AND afp_t.afp_transgene IS NULL AND afp_seq.afp_seqchange IS NULL AND afp_ge.afp_geneint IS NULL AND afp_gp.afp_geneprod IS NULL AND afp_gr.afp_genereg IS NULL AND afp_nm.afp_newmutant IS NULL AND afp_rnai.afp_rnai IS NULL AND afp_ov.afp_overexpr IS NULL AND afp_stc.afp_structcorr IS NULL AND afp_antibody.afp_antibody IS NULL AND afp_siteaction.afp_siteaction IS NULL AND afp_timeaction.afp_timeaction IS NULL AND afp_rnaseq.afp_rnaseq IS NULL AND afp_chemphen.afp_chemphen IS NULL AND afp_envpheno.afp_envpheno IS NULL AND afp_catalyticact.afp_catalyticact IS NULL AND afp_humdis.afp_humdis IS NULL AND afp_additionalexpr.afp_additionalexpr IS NULL AND afp_comment.afp_comment IS NULL "
 QUERY_PAPER_IDS_FULL_SUBMISSION = "SELECT distinct afp_lasttouched.joinkey FROM afp_lasttouched JOIN afp_version ON afp_lasttouched.joinkey = afp_version.joinkey WHERE afp_version.afp_version = '2' "
@@ -967,17 +968,18 @@ class WBAFPDBManager(AbstractWBDBManager):
 
     def get_positive_paper_ids_sumbitted_last_month_for_data_type(self, data_type_table_name, tazendra_user,
                                                                   tazendra_password):
-        curated_ids = self.get_curated_papers(data_type_table_name[4:], tazendra_user, tazendra_password)
-        self.cur.execute("SELECT {}.joinkey, {}.{} from {} join afp_lasttouched "
+        curated_ids = get_curated_papers(data_type_table_name[4:], tazendra_user, tazendra_password)
+        with self.get_cursor() as curs:
+            curs.execute("SELECT {}.joinkey, {}.{} from {} join afp_lasttouched "
                          "ON {}.joinkey = afp_lasttouched.joinkey JOIN afp_version "
                          "ON afp_lasttouched.joinkey = afp_version.joinkey "
                          "WHERE afp_version.afp_version = '2' AND {}.afp_timestamp > now() - interval '1 week' AND "
                          "{}.{} IS NOT NULL".format(
             data_type_table_name, data_type_table_name, data_type_table_name, data_type_table_name,
             data_type_table_name, data_type_table_name, data_type_table_name, data_type_table_name))
-        rows = self.cur.fetchall()
-        return {row[0]: row[1] if row[1] != "Checked" else "" for row in rows if row[0] not in curated_ids and
-                row[1] != "" and
-                row[1] != "[{\"id\":1,\"name\":\"\"}]" and
-                row[1] != "[{\"id\":1,\"name\":\"\",\"publicationId\":\"\"}]"}
+            rows = curs.fetchall()
+            return {row[0]: row[1] if row[1] != "Checked" else "" for row in rows if row[0] not in curated_ids and
+                    row[1] != "" and
+                    row[1] != "[{\"id\":1,\"name\":\"\"}]" and
+                    row[1] != "[{\"id\":1,\"name\":\"\",\"publicationId\":\"\"}]"}
 
