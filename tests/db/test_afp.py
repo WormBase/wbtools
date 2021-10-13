5b58a4c62b59d09443f97acc572f50f49cb1289e
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-13 09:49:01 -0700

    feat(afp): added method to get submission stats as time series
    - binning size can be passed as argument

---
+++ tests/db/test_afp.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+from tests.config_reader import read_db_config
+from wbtools.db.afp import WBAFPDBManager
+
+
+@unittest.skipIf(not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data",
+                                                 "local_config", "db.cfg")), "Test DB config file not present")
+class TestWBAFPDBManager(unittest.TestCase):
+
+    def setUp(self) -> None:
+        config = read_db_config()
+        self.db_manager = WBAFPDBManager(dbname=config["wb_database"]["db_name"],
+                                         user=config["wb_database"]["db_user"],
+                                         password=config["wb_database"]["db_password"],
+                                         host=config["wb_database"]["db_host"])
+
+    def test_get_monthly_stats(self):
+        monthly_stats = self.db_manager.get_monthly_stats(bin_period='m')
+        self.assertTrue(len(monthly_stats) > 0)
+        self.assertTrue(len(monthly_stats[0]) == 2)
+        self.assertTrue(len(monthly_stats[0][1]) == 5)
--- wbtools/db/afp.py
+++ wbtools/db/afp.py
@@ -1,5 +1,7 @@
 import time
 import urllib.parse
+import pandas as pd
+
 from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import List, Tuple
@@ -925,4 +927,40 @@ class WBAFPDBManager(AbstractWBDBManager):
         if limit is None:
             limit = len(sorted_list) - offset
         return sorted_list[offset: offset + limit], len(sorted_list)
+
+    @staticmethod
+    def _bin_dates(dates: List[datetime], bin_period: str = 'm'):
+        s = pd.to_datetime(pd.Series(dates), utc=True)
+        s.index = s.dt.to_period(bin_period)
+        s = s.groupby(level=0).size()
+        s = s.reindex(pd.period_range(s.index.min(), s.index.max(), freq=bin_period), fill_value=0)
+        return list(zip([tp.strftime('%Y-%m') for tp in s.index.to_list()], s.to_list()))
+
+    def get_monthly_stats(self, bin_period: str = 'm'):
+        with self.get_cursor() as curs:
+            curs.execute("SELECT distinct afp_lasttouched.joinkey, afp_version.afp_timestamp FROM afp_lasttouched "
+                         "JOIN afp_version ON afp_lasttouched.joinkey = afp_version.joinkey "
+                         "WHERE afp_version.afp_version = '2'")
+            submitted_dates = [row[1] for row in curs.fetchall()]
+            curs.execute("SELECT distinct afp_ve.joinkey, afp_ve.afp_timestamp FROM afp_version afp_ve FULL OUTER JOIN afp_lasttouched afp_l ON afp_ve.joinkey = afp_l.joinkey FULL OUTER JOIN afp_genestudied afp_g ON afp_ve.joinkey = afp_g.joinkey FULL OUTER JOIN afp_species afp_s ON afp_ve.joinkey = afp_s.joinkey FULL OUTER JOIN afp_variation afp_v ON afp_ve.joinkey = afp_v.joinkey FULL OUTER JOIN afp_strain afp_st ON afp_ve.joinkey = afp_st.joinkey FULL OUTER JOIN afp_transgene afp_t ON afp_ve.joinkey = afp_t.joinkey FULL OUTER JOIN afp_seqchange afp_seq ON afp_ve.joinkey = afp_seq.joinkey FULL OUTER JOIN afp_geneint afp_ge ON afp_ve.joinkey = afp_ge.joinkey FULL OUTER JOIN afp_geneprod afp_gp ON afp_ve.joinkey = afp_gp.joinkey FULL OUTER JOIN afp_genereg afp_gr ON afp_ve.joinkey = afp_gr.joinkey FULL OUTER JOIN afp_newmutant afp_nm ON afp_ve.joinkey = afp_nm.joinkey FULL OUTER JOIN afp_rnai afp_rnai ON afp_ve.joinkey = afp_rnai.joinkey FULL OUTER JOIN afp_overexpr afp_ov ON afp_ve.joinkey = afp_ov.joinkey FULL OUTER JOIN afp_structcorr afp_stc ON afp_ve.joinkey = afp_stc.joinkey FULL OUTER JOIN afp_antibody ON afp_ve.joinkey = afp_antibody.joinkey FULL OUTER JOIN afp_siteaction ON afp_ve.joinkey = afp_siteaction.joinkey FULL OUTER JOIN afp_timeaction ON afp_ve.joinkey = afp_timeaction.joinkey FULL OUTER JOIN afp_rnaseq ON afp_ve.joinkey = afp_rnaseq.joinkey FULL OUTER JOIN afp_chemphen ON afp_ve.joinkey = afp_chemphen.joinkey FULL OUTER JOIN afp_envpheno ON afp_ve.joinkey = afp_envpheno.joinkey FULL OUTER JOIN afp_catalyticact ON afp_ve.joinkey = afp_catalyticact.joinkey FULL OUTER JOIN afp_humdis ON afp_ve.joinkey = afp_humdis.joinkey FULL OUTER JOIN afp_additionalexpr ON afp_ve.joinkey = afp_additionalexpr.joinkey FULL OUTER JOIN afp_comment ON afp_ve.joinkey = afp_comment.joinkey WHERE afp_ve.afp_version = '2' AND afp_l.afp_lasttouched is NULL AND (afp_g.afp_genestudied IS NOT NULL OR afp_s.afp_species IS NOT NULL OR afp_v.afp_variation IS NOT NULL OR afp_st.afp_strain IS NOT NULL OR afp_t.afp_transgene IS NOT NULL OR afp_seq.afp_seqchange IS NOT NULL OR afp_ge.afp_geneint IS NOT NULL OR afp_gp.afp_geneprod IS NOT NULL OR afp_gr.afp_genereg IS NOT NULL OR afp_nm.afp_newmutant IS NOT NULL OR afp_rnai.afp_rnai IS NOT NULL OR afp_ov.afp_overexpr IS NOT NULL OR afp_stc.afp_structcorr IS NOT NULL OR afp_antibody.afp_antibody IS NOT NULL OR afp_siteaction.afp_siteaction IS NOT NULL OR afp_timeaction.afp_timeaction IS NOT NULL OR afp_rnaseq.afp_rnaseq IS NOT NULL OR afp_chemphen.afp_chemphen IS NOT NULL OR afp_envpheno.afp_envpheno IS NOT NULL OR afp_catalyticact.afp_catalyticact IS NOT NULL OR afp_humdis.afp_humdis IS NOT NULL OR afp_additionalexpr.afp_additionalexpr IS NOT NULL OR afp_comment.afp_comment IS NOT NULL)")
+            partial_dates = [row[1] for row in curs.fetchall()]
+            curs.execute("SELECT distinct afp_ve.joinkey, afp_ve.afp_timestamp FROM afp_version afp_ve FULL OUTER JOIN afp_genestudied afp_g ON afp_ve.joinkey = afp_g.joinkey FULL OUTER JOIN afp_species afp_s ON afp_ve.joinkey = afp_s.joinkey FULL OUTER JOIN afp_variation afp_v ON afp_ve.joinkey = afp_v.joinkey FULL OUTER JOIN afp_strain afp_st ON afp_ve.joinkey = afp_st.joinkey FULL OUTER JOIN afp_transgene afp_t ON afp_ve.joinkey = afp_t.joinkey FULL OUTER JOIN afp_seqchange afp_seq ON afp_ve.joinkey = afp_seq.joinkey FULL OUTER JOIN afp_geneint afp_ge ON afp_ve.joinkey = afp_ge.joinkey FULL OUTER JOIN afp_geneprod afp_gp ON afp_ve.joinkey = afp_gp.joinkey FULL OUTER JOIN afp_genereg afp_gr ON afp_ve.joinkey = afp_gr.joinkey FULL OUTER JOIN afp_newmutant afp_nm ON afp_ve.joinkey = afp_nm.joinkey FULL OUTER JOIN afp_rnai afp_rnai ON afp_ve.joinkey = afp_rnai.joinkey FULL OUTER JOIN afp_overexpr afp_ov ON afp_ve.joinkey = afp_ov.joinkey FULL OUTER JOIN afp_structcorr afp_stc ON afp_ve.joinkey = afp_stc.joinkey FULL OUTER JOIN afp_antibody ON afp_ve.joinkey = afp_antibody.joinkey FULL OUTER JOIN afp_siteaction ON afp_ve.joinkey = afp_siteaction.joinkey FULL OUTER JOIN afp_timeaction ON afp_ve.joinkey = afp_timeaction.joinkey FULL OUTER JOIN afp_rnaseq ON afp_ve.joinkey = afp_rnaseq.joinkey FULL OUTER JOIN afp_chemphen ON afp_ve.joinkey = afp_chemphen.joinkey FULL OUTER JOIN afp_envpheno ON afp_ve.joinkey = afp_envpheno.joinkey FULL OUTER JOIN afp_catalyticact ON afp_ve.joinkey = afp_catalyticact.joinkey FULL OUTER JOIN afp_humdis ON afp_ve.joinkey = afp_humdis.joinkey FULL OUTER JOIN afp_additionalexpr ON afp_ve.joinkey = afp_additionalexpr.joinkey FULL OUTER JOIN afp_comment ON afp_ve.joinkey = afp_comment.joinkey WHERE afp_ve.afp_version = '2' AND afp_g.afp_genestudied IS NULL AND afp_s.afp_species IS NULL AND afp_v.afp_variation IS NULL AND afp_st.afp_strain IS NULL AND afp_t.afp_transgene IS NULL AND afp_seq.afp_seqchange IS NULL AND afp_ge.afp_geneint IS NULL AND afp_gp.afp_geneprod IS NULL AND afp_gr.afp_genereg IS NULL AND afp_nm.afp_newmutant IS NULL AND afp_rnai.afp_rnai IS NULL AND afp_ov.afp_overexpr IS NULL AND afp_stc.afp_structcorr IS NULL AND afp_antibody.afp_antibody IS NULL AND afp_siteaction.afp_siteaction IS NULL AND afp_timeaction.afp_timeaction IS NULL AND afp_rnaseq.afp_rnaseq IS NULL AND afp_chemphen.afp_chemphen IS NULL AND afp_envpheno.afp_envpheno IS NULL AND afp_catalyticact.afp_catalyticact IS NULL AND afp_humdis.afp_humdis IS NULL AND afp_additionalexpr.afp_additionalexpr IS NULL AND afp_comment.afp_comment IS NULL")
+            waiting_dates = [row[1] for row in curs.fetchall()]
+            submitted_dates_binned = self._bin_dates(submitted_dates, bin_period)
+            partial_dates_binned = self._bin_dates(partial_dates, bin_period)
+            waiting_dates_binned = self._bin_dates(waiting_dates, bin_period)
+            monthly_stats_dict = defaultdict(lambda: [0] * 5)
+            for sub_date_bin in submitted_dates_binned:
+                monthly_stats_dict[sub_date_bin[0]][0] += sub_date_bin[1]
+                monthly_stats_dict[sub_date_bin[0]][1] = sub_date_bin[1]
+            for sub_date_bin in partial_dates_binned:
+                monthly_stats_dict[sub_date_bin[0]][0] += sub_date_bin[1]
+                monthly_stats_dict[sub_date_bin[0]][2] = sub_date_bin[1]
+            for sub_date_bin in waiting_dates_binned:
+                monthly_stats_dict[sub_date_bin[0]][0] += sub_date_bin[1]
+                monthly_stats_dict[sub_date_bin[0]][3] = sub_date_bin[1]
+            for period, stats in monthly_stats_dict.items():
+                monthly_stats_dict[period][4] = (stats[1] / stats[0]) if stats[0] > 0 else 0
+            return sorted([(period, stats) for period, stats in monthly_stats_dict.items()], key=lambda x: x[0])
+
 
