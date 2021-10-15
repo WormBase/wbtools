28cbe0f346bec3021420f6d833b94a0dbbe11460
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-14 18:21:15 -0700

    fix(afp): changed stats to exclude papers without afp_email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wbtools",
-    version="1.2.11",
+    version="1.2.12",
     author="Valerio Arnaboldi",
     author_email="valearna@caltech.edu",
     description="Interface to WormBase (www.wormbase.org) curation data, including literature management and NLP "
