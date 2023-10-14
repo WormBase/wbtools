c27e3c5b55e20291e2374608ec7a60cc230176cb
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-10-14 15:16:13 -0700

    Update version and SQL query in setup.py and generic.py
    Increased version number from 2.0.1 to 2.0.2 in setup.py to reflect recent changes. In generic.py, modified the SQL query to select from 'pap_title' instead of 'pap_electronic_path' for better dataset retrieval based on timestamp.

--- wbtools/db/generic.py
+++ wbtools/db/generic.py
@@ -23,7 +23,7 @@ class WBGenericDBManager(AbstractWBDBManager):
         if not added_or_modified_after:
             added_or_modified_after = '1970-01-01'
         with self.get_cursor() as curs:
-            curs.execute("SELECT DISTINCT joinkey, pap_timestamp from pap_electronic_path WHERE pap_timestamp > %s "
+            curs.execute("SELECT DISTINCT joinkey, pap_timestamp from pap_title WHERE pap_timestamp > %s "
                          "ORDER BY pap_timestamp DESC",
                          (added_or_modified_after, ))
             res = curs.fetchall()
