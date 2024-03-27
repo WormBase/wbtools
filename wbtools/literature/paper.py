d80af48a6f2c266fa3f7107ca4aa35df9290a42c
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-03-26 18:06:17 -0700

    Removing xml tags from sentences

--- wbtools/literature/paper.py
+++ wbtools/literature/paper.py
@@ -49,8 +49,8 @@ def convert_pdf_to_txt(file_path):
             r = process_fulltext_document.sync_detailed(client=client, multipart_data=form)
             if r.is_success:
                 article: Article = TEI.parse(r.content, figures=False)
-                return [sentence.text for section in article.sections for paragraph in section.paragraphs
-                        for sentence in paragraph if section.name is not None]
+                return [re.sub('<[^<]+>', '', sentence.text) for section in article.sections for paragraph
+                        in section.paragraphs for sentence in paragraph if section.name is not None]
             else:
                 return []
     except:
